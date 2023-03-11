76fe620f6532b9f17b209b18e0376f5864b9ab10
Carlos Hortua <c-y-h-c702@hotmail.com>
2023-03-11 23:28:20 +0100

    Added feature to filter Pokémon list by name

--- app/src/main/java/co/cahoca/pokedex/PokedexListActivity.kt
+++ app/src/main/java/co/cahoca/pokedex/PokedexListActivity.kt
@@ -5,7 +5,10 @@ import android.graphics.BitmapFactory
 import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import android.util.Log
+import android.widget.EditText
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doOnTextChanged
 import androidx.recyclerview.widget.RecyclerView
 import co.cahoca.pokedex.adapter.PokemonListAdapter
 import co.cahoca.pokedex.data.DataSource
@@ -21,12 +24,28 @@ class PokedexListActivity : AppCompatActivity() {
 
 
         val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
-        recyclerView.setHasFixedSize(true)
+        //recyclerView.setHasFixedSize(true)
 
         val dataset: List<Pokemon> = DataSource().loadPokemonMonsters()
         recyclerView.adapter = PokemonListAdapter(this, dataset)
+
+        val editText = findViewById<EditText>(R.id.pokedex_list_view_filter_text)
+        val imageButton = findViewById<ImageButton>(R.id.pokedex_list_view_filter_clear)
+        imageButton.setOnClickListener { editText.setText("") }
+
+        editText.doOnTextChanged{searchText, _, _, _ ->
+            if(searchText != null){
+                refreshList(recyclerView,
+                            dataset.filter { pokemon -> pokemon.name.contains(searchText,true) })
+            }else{
+                refreshList(recyclerView,dataset)
+            }
+        }
     }
 
+    private fun refreshList(recyclerView: RecyclerView, filteredDataSet: List<Pokemon>) {
+        recyclerView.adapter = PokemonListAdapter(this, filteredDataSet)
+    }
     companion object{
         fun getAssetsDrawable(context: Context, fileName:String): BitmapDrawable?{
             var bitmap: BitmapDrawable? = null
--- app/src/main/java/co/cahoca/pokedex/data/DataSource.kt
+++ app/src/main/java/co/cahoca/pokedex/data/DataSource.kt
@@ -5,23 +5,23 @@ import co.cahoca.pokedex.model.*
 class DataSource {
     fun loadPokemonMonsters(): List<Pokemon>{
         return listOf(
-            Pokemon(1, "Bulbasaur", "A strange seed was planted on its back at birth. The plant sprouts\n" +
-                    "and grows with this Pokémon", Type.GRASS, Type.POISON),
-            Pokemon(2, "Ivysaur", "When the bulb on its back grows large, it appears to lose the ability\n" +
+            Pokemon(1, "Bulbasaur", "A strange seed was planted on its back at birth. The plant sprouts" +
+                    " and grows with this Pokémon", Type.GRASS, Type.POISON),
+            Pokemon(2, "Ivysaur", "When the bulb on its back grows large, it appears to lose the ability " +
                     "to stand on its hind legs", Type.GRASS, Type.POISON),
-            Pokemon(3, "Venusaur", "The plant blooms when it is asorbing solar energy. It stays on the\n" +
+            Pokemon(3, "Venusaur", "The plant blooms when it is asorbing solar energy. It stays on the " +
                     "move to seek sunlight", Type.GRASS, Type.POISON),
-            Pokemon(4, "Charmander", "Obviously prefers hot places. When it rains, steam is said to spout\n" +
+            Pokemon(4, "Charmander", "Obviously prefers hot places. When it rains, steam is said to spout " +
                     "from the tip of its tail.", Type.FIRE, null),
-            Pokemon(5, "Charmeleon", "When it swings its burning tail, it elevates the temperature to\n" +
+            Pokemon(5, "Charmeleon", "When it swings its burning tail, it elevates the temperature to " +
                     "unbearably high levels.", Type.FIRE, null),
-            Pokemon(6, "Charizard", "It spits fire that is hot enough to melt boulders. Known to cause\n" +
+            Pokemon(6, "Charizard", "It spits fire that is hot enough to melt boulders. Known to cause " +
                     "forest fires unintentionally.", Type.FIRE, Type.FLYING),
-            Pokemon(7, "Squirtle", "After birth, its back swells and hardens into a shell. Powerfully\n" +
+            Pokemon(7, "Squirtle", "After birth, its back swells and hardens into a shell. Powerfully " +
                     "sprays foam from its mouth.", Type.WATER, null),
-            Pokemon(9, "Wartortle", "Often hides in water to stalk unwary prey. For swimming fast, it\n" +
+            Pokemon(9, "Wartortle", "Often hides in water to stalk unwary prey. For swimming fast, it " +
                     "moves its ears to maintain balance.", Type.WATER, null),
-            Pokemon(10, "Blastoise", "A brutal Pokémon with pressurized water jets on its shell. They are\n" +
+            Pokemon(10, "Blastoise", "A brutal Pokémon with pressurized water jets on its shell. They are " +
                     "used for high speed tackles.", Type.WATER, null),
         );
     }
