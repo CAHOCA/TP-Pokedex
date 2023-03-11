5a5145276e6e0312925cb50d3fe6f956e58b5f55
Carlos Hortua Calderon <43701590+CAHOCA@users.noreply.github.com>
2023-03-11 22:51:37 +0100

    Dev feature (#5)
    * Add pokémon img assets
    
    * Rename view Main Activity
    
    * Pkémon types enum
    
    * DataSource and Add new Pokémon Types
    
    * Pokémon list Activities
    
    * Logic Main Activity and change Manifest
    
    * Android Manifest refactor
    
    * Change App Icon
    
    * Pokédex List Activity & List Adapter
    
    * Added pokédex list activity

--- app/src/main/java/co/cahoca/pokedex/PokedexListActivity.kt
+++ app/src/main/java/co/cahoca/pokedex/PokedexListActivity.kt
@@ -7,6 +7,10 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
+import co.cahoca.pokedex.adapter.PokemonListAdapter
+import co.cahoca.pokedex.data.DataSource
+import co.cahoca.pokedex.model.Pokemon
+
 import java.io.IOException
 
 const val TAG: String = "PokedexListActivity"
@@ -17,13 +21,26 @@ class PokedexListActivity : AppCompatActivity() {
         setContentView(R.layout.activity_pokedex_list)
 
         val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
-
         recyclerView.setHasFixedSize(true)
+
+        val dataset: List<Pokemon> = DataSource().loadPokemonMonsters()
+        recyclerView.adapter = PokemonListAdapter(this, dataset)
+
     }
 
     companion object{
         fun getAssetsDrawable(context: Context, fileName:String): BitmapDrawable?{
             var bitmap: BitmapDrawable? = null
+            try
+            {
+                val inputStream = context.assets.open(fileName)
+                bitmap = BitmapDrawable(context.resources,
+                    BitmapFactory.decodeStream(inputStream))
+            } catch (e: IOException)
+            {
+                Log.e(TAG, "Error occurred retrieving file $fileName - ${e.message}")
+                e.printStackTrace()
+            }
             return bitmap
         }
     }
